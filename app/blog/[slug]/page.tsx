f4c8fbc2575b893fe5f81c4f7dd69cda6f42dc42
Kalema Rogers <rogerskalema0@gmail.com>
2025-09-18 12:50:22 +0300

    Fixed prames error

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -12,7 +12,7 @@ export async function generateStaticParams() {
 }
 
 export async function generateMetadata({ params }: { params: { slug: string } }) {
-  const { slug } = await params;
+  const { slug } = params;
   let post = getBlogPosts().find((post) => post.slug === slug);
   if (!post) {
     return;
@@ -53,7 +53,7 @@ export async function generateMetadata({ params }: { params: { slug: string } })
 }
 
 export default async function Blog({ params }: { params: { slug: string } }) {
-  const { slug } = await params;
+  const { slug } = params;
   let post = getBlogPosts().find((post: { slug: string }) => post.slug === slug);
 
   if (!post) {
